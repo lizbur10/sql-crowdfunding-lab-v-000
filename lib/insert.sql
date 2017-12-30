dc364bb76d0ecac89c1358743eb2aea8dadfefda
Liz Burton <liz.burton147@gmail.com>
2017-12-30 22:15:02 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Exploding Kittens", "Board Game", 10000, "1/1/2015", "2/19/2015");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Golem", "Blockchain", 8600000, "11/11/2016", "11/11/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Ouya", "Video Game", 950000, "7/1/2012", "8/9/2012");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Shenmue III", "Video Game", 2000000, "7/16/2015", "Ongoing");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("FirstBlood Crowdsale", "Software", 5500000, "9/26/2016", "9/26/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Pono Music", "Computing Hardware", 800000, "4/1/2014", "4/15/2014");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Mayday PAC", "Other", 6000000, "5/1/2014", "7/4/2014");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Fidget Cube", "Other", 15000, "10/15/2016", "10/19/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Veronica Mars", "Movie", 2000000, "4/1/2013", "4/13/2013");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Lisk", "Software", NULL, "2/29/2016", "3/21/2016");
